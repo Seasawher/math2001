d638469a3e6c071efd7660351e29921c4f07614c
Seasawher <tellagoodstory@icloud.com>
2023-08-04 19:42:01 +0900

    solve exercise 1.4

--- Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
+++ Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
@@ -21,14 +21,18 @@ example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 :=
 -- Exercise: replace the words "sorry" with the correct Lean justification.
 example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
   calc
-    r = (s + r + r - s) / 2 := by sorry
-    _ ≤ (3 + (s + 3) - s) / 2 := by sorry
-    _ = 3 := by sorry
+    r = (s + r + r - s) / 2 := by ring
+    _ ≤ (3 + (s + 3) - s) / 2 := by rel [h1, h2]
+    _ = 3 := by ring
 
 -- Example 1.4.3
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
 example {x y : ℝ} (h1 : y ≤ x + 5) (h2 : x ≤ -2) : x + y < 2 :=
-  sorry
+  calc
+    x + y ≤ x + (x + 5) := by rel [h1]
+    _ = 2 * x + 5 := by ring
+    _ ≤ 2 * (-2) + 5 := by rel [h2]
+    _ < 2 := by numbers
 
 -- Example 1.4.4
 -- Exercise: replace the words "sorry" with the correct Lean justification.
@@ -49,11 +53,11 @@ example {u v x y A B : ℝ} (h1 : 0 < A) (h2 : A ≤ 1) (h3 : 1 ≤ B) (h4 : x �
 example {t : ℚ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 :=
   calc
     t ^ 2 - 3 * t - 17
-      = t * t - 3 * t - 17 := by sorry
-    _ ≥ 10 * t - 3 * t - 17 := by sorry
-    _ = 7 * t - 17 := by sorry
-    _ ≥ 7 * 10 - 17 := by sorry
-    _ ≥ 5 := by sorry
+      = t * t - 3 * t - 17 := by ring
+    _ ≥ 10 * t - 3 * t - 17 := by rel [ht]
+    _ = 7 * t - 17 := by ring
+    _ ≥ 7 * 10 - 17 := by rel [ht]
+    _ ≥ 5 := by numbers
 
 -- Example 1.4.6
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
@@ -107,10 +111,17 @@ up in Lean. -/
 
 
 example {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 :=
-  sorry
+  calc
+    x = (x + 3) - 3 := by ring
+    _ ≥ 2 * y - 3 := by rel [h1]
+    _ ≥ 2 * 1 - 3 := by rel [h2]
+    _ = -1 := by ring
 
 example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
-  sorry
+  calc
+    a + b = ((a + 2 * b) + a) / 2 := by ring
+    _ ≥ (4 + 3) / 2 := by rel [h1, h2]
+    _ ≥ 3 := by numbers
 
 example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
   sorry
@@ -122,7 +133,13 @@ example {n : ℤ} (h1 : n ≥ 5) : n ^ 2 - 2 * n + 3 > 14 :=
   sorry
 
 example {x : ℚ} : x ^ 2 - 2 * x ≥ -1 :=
-  sorry
+  calc
+    x ^ 2 - 2 * x = (x - 1) ^ 2 - 1 := by ring
+    _ ≥ -1 := by extra
 
 example (a b : ℝ) : a ^ 2 + b ^ 2 ≥ 2 * a * b :=
-  sorry
+  calc
+    a ^ 2 + b ^ 2 = a ^ 2 + b ^ 2 + 2 * a * b - 2 * a * b := by ring
+    _ = (a - b) ^ 2 + 2 * a * b := by ring
+    _ ≥ 0 + 2 * a * b := by extra
+    _ = 2 * a * b := by ring
